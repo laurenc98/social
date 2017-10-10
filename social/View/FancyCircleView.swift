5c8e3119edff9b25d52a94a87aee254cf8896e62
Lauren Charlton <laurencharlton@Laurens-MacBook-Pro.local>
2017-10-10 15:46:15 +0100

    UI for feed done

--- social/View/FancyCircleView.swift
+++ social/View/FancyCircleView.swift
@@ -1,18 +1,16 @@
 //
-//  FancyView.swift
+//  FancyCircleView.swift
 //  social
 //
-//  Created by Lauren Charlton on 06/10/2017.
+//  Created by Lauren Charlton on 10/10/2017.
 //  Copyright © 2017 Lauren Charlton. All rights reserved.
 //
 
 import UIKit
-//Change the look of the header view
-class FancyView: UIView {
-    //Needed
+//circle the images
+class FancyCircleView: UIImageView {
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         //Shadow effect
         //Shadow colour setting. It uses the SHADOW_GREY constant from the constants file and sets it as a grey colour
         layer.shadowColor = UIColor(red: SHADOW_GREY, green: SHADOW_GREY, blue: SHADOW_GREY, alpha: 0.6).cgColor
@@ -22,8 +20,12 @@ class FancyView: UIView {
         layer.shadowRadius = 5.0
         //Shadow offset (where the shadow is located from the view)
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        //Corner radius
-        layer.cornerRadius = 2.0
-        
+    }
+    
+    //function for editing the view. Will be used to change the image shape to circle
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        //The function helps include the frame size which can be used in the calculation
+        layer.cornerRadius = self.frame.width / 2
     }
 }
